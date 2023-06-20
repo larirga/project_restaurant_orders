d1482d294be9f47978c7545caf8c49f06b49d727
Larissa <larisssa_rga@hotmail.com>
2023-06-20 15:25:04 -0300

    [add] - req1

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,18 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 
-# Req 1 um
+# Req 1 
 def test_ingredient():
-    pass
+    ingredient = Ingredient('queijo mussarela')
+    ingredient2 = Ingredient('frango')
+    ingredient3 = Ingredient('')
+
+    assert ingredient == ingredient
+    assert ingredient != ingredient2
+    assert ingredient3.name == ''
+    assert ingredient.name == "queijo mussarela"
+    assert ingredient.restrictions
+
+    assert hash(ingredient) == hash(ingredient)
+    assert hash(ingredient) != hash(ingredient2)
+    assert repr(ingredient) == "Ingredient('queijo mussarela')"
